f7f01e6c3d5b97e5291dd54cf36b86df76605f0a
Dmitry Zhukovsky <dmitry.zhukovsky@solgari.com>
2025-03-03 02:37:58 +0100

    Update version to 1.0.3, refactor App component, and remove unused TableExample component

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,14 @@ import "azure-devops-ui/Core/override.css";
 
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App.tsx";
+import { PipelinesListingPageExample } from "./App.tsx";
 import * as SDK from "azure-devops-extension-sdk";
-import { SurfaceBackground, SurfaceContext } from "azure-devops-ui/Surface";
 
 SDK.init();
 
 ReactDOM.render(
   <StrictMode>
-    <SurfaceContext.Provider value={{ background: SurfaceBackground.neutral }}>
-      <App />
-    </SurfaceContext.Provider>
+    <PipelinesListingPageExample />
   </StrictMode>,
   document.getElementById("root")
 );
